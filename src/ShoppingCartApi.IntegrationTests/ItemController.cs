51185843a39313fc05e93916a2d2c0e747d4abd1
Nojj02 <nojj02@gmail.com>
2019-03-02 16:53:43 +0800

    Added contRewrote acceptance test for first requirement

--- src/ShoppingCartApi.IntegrationTests/ItemController.cs
+++ src/ShoppingCartApi.IntegrationTests/ItemController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,18 +9,81 @@ using Xunit;
 
 namespace ShoppingCartApi.IntegrationTests
 {
-    public class ItemController
+    public class ComputeCostTests
     {
         [Fact]
         public async Task CreateNewItem()
         {
+            await GivenAShopWithItems(
+                new List<dynamic>
+                {
+                    new {Code = "potato", Price = 30},
+                    new {Code = "apple", Price = 70},
+                    new {Code = "tomato", Price = 50},
+                });
+
+            await UserCanComputeTotalCostOfShoppingItems(
+                shoppingItems: new List<dynamic>
+                {
+                    new {ItemCode = "potato", Quantity = 3},
+                    new {ItemCode = "apple", Quantity = 1}
+                },
+                expectedTotalCost: 160);
+        }
+
+        private async Task UserCanComputeTotalCostOfShoppingItems(
+            List<dynamic> shoppingItems,
+            decimal expectedTotalCost)
+        {
+            var computeCostDto =
+                new
+                {
+                    ShoppingItems =
+                        shoppingItems
+                            .Select(x =>
+                                new
+                                {
+                                    ItemCode = x.ItemCode,
+                                    Quantity = x.Quantity
+                                })
+                };
+
+            var httpContent = new StringContent(JsonConvert.SerializeObject(computeCostDto));
+            var postRequestMessage =
+                new HttpRequestMessage(
+                    method: HttpMethod.Post,
+                    requestUri: new Uri("http://localhost:9050/calculator/computeCost"))
+                {
+                    Content = httpContent
+                };
+            
             using (var httpClient = new HttpClient())
             {
-                var item = new
+                var postResponse = await httpClient.SendAsync(postRequestMessage);
+                
+                Assert.Equal(HttpStatusCode.OK, postResponse.StatusCode);
+
+                var body = 
+                    JsonConvert.DeserializeAnonymousType(await postResponse.Content.ReadAsStringAsync(),
+                        new
+                        {
+                            TotalCost = 0.0m
+                        });
+                
+                Assert.Equal(expectedTotalCost, body.TotalCost);
+            }
+        }
+
+        private async Task GivenAShopWithItems(List<dynamic> items)
+        {
+            foreach (var item in items)
+            {
+                var itemDto = new
                 {
-                    Code = "potato"
+                    item.Code
                 };
-                var httpContent = new StringContent(JsonConvert.SerializeObject(item));
+
+                var httpContent = new StringContent(JsonConvert.SerializeObject(itemDto));
 
                 var postRequestMessage =
                     new HttpRequestMessage(
@@ -27,27 +92,13 @@ namespace ShoppingCartApi.IntegrationTests
                     {
                         Content = httpContent
                     };
-
-                var postResult = await httpClient.SendAsync(postRequestMessage);
                 
-                Assert.Equal(HttpStatusCode.Created, postResult.StatusCode);
+                using (var httpClient = new HttpClient())
+                {
+                    var postResponse = await httpClient.SendAsync(postRequestMessage);
 
-                var postResultObject = JsonConvert.DeserializeAnonymousType(await postResult.Content.ReadAsStringAsync(),
-                    new
-                    {
-                        Code = ""
-                    });
-
-                var getRequestMessage =
-                    new HttpRequestMessage(
-                        method: HttpMethod.Get,
-                        requestUri: new Uri($"http://localhost:9050/items/{postResultObject.Code}"))
-                    {
-                        Content = httpContent
-                    };
-
-                var getResult = await httpClient.SendAsync(getRequestMessage);
-                Assert.Equal(HttpStatusCode.OK, getResult.StatusCode);
+                    Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+                }
             }
         }
     }
