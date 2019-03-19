345dce82481c391798418475686ac1db0a0187b0
Jonn <nojj02@gmail.com>
2019-03-20 00:20:34 +0800

    Refactored into steps

---
+++ src/ShoppingCartApi.IntegrationTests/Steps.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ShoppingCartApi.IntegrationTests
+{
+    public static class Steps
+    {
+        public static async Task UserCanComputeTotalCostOfShoppingItems(
+            List<dynamic> shoppingItems,
+            decimal expectedTotalCost)
+        {
+            var computeCostDto =
+                new
+                {
+                    ShoppingItems =
+                        shoppingItems
+                            .Select(x =>
+                                new
+                                {
+                                    ItemCode = x.ItemCode,
+                                    Quantity = x.Quantity
+                                })
+                };
+
+            var httpContent = new StringContent(JsonConvert.SerializeObject(computeCostDto), Encoding.UTF8, "application/json");
+            var postRequestMessage =
+                new HttpRequestMessage(
+                    method: HttpMethod.Post,
+                    requestUri: new Uri("http://localhost:9050/calculator/computeCost"))
+                {
+                    Content = httpContent
+                };
+            
+            using (var httpClient = new HttpClient())
+            {
+                var postResponse = await httpClient.SendAsync(postRequestMessage);
+                
+                Assert.Equal(HttpStatusCode.OK, postResponse.StatusCode);
+
+                var body = 
+                    JsonConvert.DeserializeAnonymousType(await postResponse.Content.ReadAsStringAsync(),
+                        new
+                        {
+                            TotalCost = 0.0m
+                        });
+                
+                Assert.Equal(expectedTotalCost, body.TotalCost);
+            }
+        }
+
+        public static async Task GivenAShopWithItems(List<dynamic> items)
+        {
+            foreach (var item in items)
+            {
+                var itemDto = new
+                {
+                    Code = item.Code,
+                    Price = item.Price
+                };
+
+                var httpContent = new StringContent(JsonConvert.SerializeObject(itemDto), Encoding.UTF8, "application/json");
+
+                var postRequestMessage =
+                    new HttpRequestMessage(
+                        method: HttpMethod.Post,
+                        requestUri: new Uri("http://localhost:9050/items"))
+                    {
+                        Content = httpContent
+                    };
+                
+                using (var httpClient = new HttpClient())
+                {
+                    var postResponse = await httpClient.SendAsync(postRequestMessage);
+
+                    Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+                }
+            }
+        }
+    }
+}
