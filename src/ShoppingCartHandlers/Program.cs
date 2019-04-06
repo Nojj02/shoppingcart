16ec42172c0201c1b8437d5a12d70fe6ccefb169
Jonn <nojj02@gmail.com>
2019-04-07 00:04:37 +0800

    Added tests for a simple event monitor

---
+++ src/ShoppingCartHandlers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingCartHandlers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
