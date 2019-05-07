d68c7f3729c917479aa04a7bf1c88f795569900b
jonnl <jonnl@pageuppeople.com>
2019-05-07 16:46:46 +0800

    Added a simple implementation for handlers program

--- src/ShoppingCartHandlers/Program.cs
+++ src/ShoppingCartHandlers/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ShoppingCartHandlers.DataAccess;
+using ShoppingCartHandlers.Web;
 
 namespace ShoppingCartHandlers
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClientWrapper = new HttpClientWrapper();
+            var eventConverter = new EventConverter(new Dictionary<string, Type>
+            {
+                { "", typeof(object) }
+            });
+            var eventApi = new EventApi(
+                host: "http://localhost:9050",
+                httpClientWrapper: httpClientWrapper,
+                eventConverter: eventConverter,
+                batchSize: 10
+            );
+
+            var eventMonitor = new EventMonitor(eventApi:eventApi, eventTrackingRepository: (IEventTrackingRepository)null);
+            await eventMonitor.Poll();
+
+            var timer = new Timer(async state =>
+                    {
+                        await eventMonitor.Poll();
+                    }, 
+                state: null,
+                period: TimeSpan.FromSeconds(5000), 
+                dueTime: TimeSpan.FromSeconds(0));
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
         }
     }
 }
